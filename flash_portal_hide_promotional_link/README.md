9f1786de4fbfaa8ba764566320ff960cd7978785
FoxOdoo <foxodoo@gmail.com>
2021-01-08 12:58:07 +0100

    Version 12

--- flash_portal_hide_promotional_link/README.md
+++ flash_portal_hide_promotional_link/README.md
@@ -5,4 +5,4 @@ With this mod it is possible to hide the two promotional link
 in the Portal purchase invoice view. On
 the left and bottom right.
 
-Odoo 13.0.
+Odoo 12.0.
--- flash_portal_hide_promotional_link/__manifest__.py
+++ flash_portal_hide_promotional_link/__manifest__.py
@@ -21,7 +21,7 @@
     'name': 'Portal Hide Promotional Link',
     'summary': 'Hide Odoo promotional link in website sale orders and bottom right link',
     'category': 'Website',
-    'version': '13.0.1.0.1',
+    'version': '12.0.1.0.1',
     'author': 'FlashOdoo',
     'website': 'https://www.flashodoo.com',
     'license': 'AGPL-3',

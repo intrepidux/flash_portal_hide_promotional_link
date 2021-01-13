2cea369af2d3176b2afc8bd99343c62fb8dfca1a
FoxOdoo <foxodoo@gmail.com>
2021-01-13 16:22:12 +0100

    Arreglo del error en v12

--- flash_portal_hide_promotional_link/__manifest__.py
+++ flash_portal_hide_promotional_link/__manifest__.py
@@ -21,7 +21,7 @@
     'name': 'Portal Hide Promotional Link',
     'summary': 'Hide Odoo promotional link in website sale orders and bottom right link',
     'category': 'Website',
-    'version': '12.0.1.0.1',
+    'version': '12.0.1.0.2',
     'author': 'FlashOdoo',
     'website': 'https://www.flashodoo.com',
     'license': 'AGPL-3',

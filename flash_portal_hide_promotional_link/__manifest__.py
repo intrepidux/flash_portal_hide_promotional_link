d53c98d52cc151a502429d83d473b6e04df2f574
FoxOdoo <76999365+FoxOdoo@users.noreply.github.com>
2022-02-01 10:32:02 +0100

    Update __manifest__.py

--- flash_portal_hide_promotional_link/__manifest__.py
+++ flash_portal_hide_promotional_link/__manifest__.py
@@ -21,7 +21,7 @@
     'name': 'Portal Hide Promotional Link',
     'summary': 'Hide Odoo promotional link in website sale orders and bottom right link',
     'category': 'Website',
-    'version': '15.0.1.0.1',
+    'version': '15.0.22.02.01',
     'author': 'FlashOdoo',
     'website': 'https://www.flashodoo.com',
     'license': 'AGPL-3',
